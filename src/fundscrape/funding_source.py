3820233ec06003d3d92c44fc20b6c66a18ec0319
Ashley Mills <ashley@ashleymills.com>
2026-07-05 08:43:03 +0100

    Removed unused classes

--- src/fundscrape/funding_source.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class FundingSource:
-    sid : str
-    url : str
-    scraper_strategy : str
--- src/fundscrape/funding_sources.py
+++
@@ -1,9 +0,0 @@
-from fundscrape.funding_source import FundingSource
-
-FUNDING_SOURCES = [
-    FundingSource(
-       sid="nihr",
-       url="https://www.nihr.ac.uk/funding-opportunities",
-       scraper_strategy="default" 
-    )
-]
